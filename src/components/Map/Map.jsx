51a08136553a364c241676230f2241b9cca674c9
imlakshaychauhan <imlakshaychauhan@gmail.com>
2022-07-09 13:13:26 +0530

    Header Completed

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,4 +1,8 @@
 import React from 'react'
+import googleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery }  from '@material-ui/core';
+import { LocationCityOutlinedIcon } from '@material-ui/icons/LocationCityOutlined';
+import Rating from '@material-ui/lab';
 
 const Map = () => {
   return (
